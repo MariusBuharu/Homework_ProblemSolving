e2162b31dcfef7f010e198ed60286557f6b0d50a
Marius Buharu <mariusbuharu@gmail.com>
2022-11-05 15:36:59 +0200

    add div separator

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 const btnReverse = document.querySelector('.reverseText');
 const container = document.querySelector('.container');
 const inputText = document.querySelector("#inputName");
-const div = document.createElement('div');
-const divapp=document.querySelector(".app");
-div.classList.add('person-details', 'mt-3', 'bg-dark', 'text-light', 'p-2');
+
 const reverse = sir => Array.from(sir).reverse().join("");
 const palindrom = pal => pal === reverse(pal);
 const bytesCode= code => {
@@ -19,18 +17,22 @@ const bytesCode= code => {
  const totalsum= suma => suma.reduce((prev,cur) => prev+cur,0);
 
 btnReverse.addEventListener('click', function () {
-
+    const div = document.createElement('div');
+    div.classList.add('person-details', 'mt-3', 'bg-dark', 'text-light', 'p-2');
+    const div2=document.createElement("div");
     const para = document.createElement("p");
     const para1 = document.createElement("p");
     const para2 = document.createElement("p");
     const para3 = document.createElement("p");
     const text = inputText.value;
     para.innerText = ` Textul inversat este :${reverse(text)}`;
-    para1.innerText=`Textul este palimdrom: ${palindrom(text)}`;
+    // para1.innerText=`Textul este palindrom: ${palindrom(text)}`;
+    para1.innerText=`${text} ${palindrom(text) ? "este palindrom":"nu este palindrom"}`;
     para2.innerText=`Secventa de bytes a textului este : ${bytesCode(text)}`;
     para3.innerText=`Suma secventei de bytes a textului este : ${totalsum(bytesCode(text))}`;
     div.append(para,para1,para2,para3);
     container.appendChild(div);
+
 });
 
 console.log(palindrom("anaytr"));
